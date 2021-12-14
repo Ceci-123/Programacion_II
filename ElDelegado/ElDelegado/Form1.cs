72a73f991d19031a74203327c6652b6e7cbb3bdf
Ceci <78119165+Ceci-123@users.noreply.github.com>
2021-12-13 22:42:44 -0300

    comparador-delegados

---
+++ ElDelegado/ElDelegado/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ElDelegado
+{
+    public partial class Form1 : Form
+    {
+        private FrmTestDelegados frmTestDelegados;
+        private FrmMostrar frmMostrar;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm1_Load(object sender, EventArgs e)
+        {
+            frmMostrar = new FrmMostrar();
+            frmMostrar.MdiParent = this;
+            frmTestDelegados = new FrmTestDelegados(frmMostrar.ActualizarNombre);
+            frmTestDelegados.MdiParent = this;
+        }
+
+        private void mostrarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                frmMostrar.Show();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("problema");
+            }
+            
+        }
+
+        private void testDelegadosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                frmTestDelegados.Show();
+                
+                mostrarToolStripMenuItem.Enabled = true;
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("problema");
+            }
+            
+        }
+    }
+}
